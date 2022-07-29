0fdcf73dd2182741ce21b7ae74323b6e03968f87
mirkokral <mirkogodak@gmail.com>
2022-07-29 07:37:32 +0100

    liteflayer v0.3

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const liteflayer = require('./liteflayer')
 const config = require('yaml').parse(require('fs').readFileSync('./config.yml','utf-8'))
-console.log(config)
+var bots = []
--- liteflayer.js
+++ liteflayer.js
@@ -1,137 +1,20 @@
-const mc = require("minecraft-protocol"),
-      fs = require("fs")
-    util = require("util")
-    rand = require("randomstring")
-    const language = require("minecraft-data")("1.16.1").language;
-
-const colormap = {
-    black: "§0",
-    dark_blue: "§1",
-    dark_green: "§2",
-    dark_aqua: "§3",
-    dark_red: "§4",
-    dark_purple: "§5",
-    gold: "§6",
-    gray: "§7",
-    dark_gray: "§8",
-    blue: "§9",
-    green: "§a",
-    aqua: "§b",
-    red: "§c",
-    light_purple: "§d",
-    yellow: "§e",
-    white: "§f",
-    reset: "§r"
-};
-
-const ansimap = {
-    "§0": "\x1b[0m\x1b[30m",
-    "§1": "\x1b[0m\x1b[34m",
-    "§2": "\x1b[0m\x1b[32m",
-    "§3": "\x1b[0m\x1b[36m",
-    "§4": "\x1b[0m\x1b[31m",
-    "§5": "\x1b[0m\x1b[35m",
-    "§6": "\x1b[0m\x1b[33m",
-    "§7": "\x1b[0m\x1b[37m",
-    "§8": "\x1b[0m\x1b[90m",
-    "§9": "\x1b[0m\x1b[94m",
-    "§a": "\x1b[0m\x1b[92m",
-    "§b": "\x1b[0m\x1b[96m",
-    "§c": "\x1b[0m\x1b[91m",
-    "§d": "\x1b[0m\x1b[95m",
-    "§e": "\x1b[0m\x1b[93m",
-    "§f": "\x1b[0m\x1b[97m",
-    "§r": "\x1b[0m",
-    "§l": "\x1b[1m",
-    "§o": "\x1b[3m",
-    "§n": "\x1b[4m",
-    "§m": "\x1b[9m",
-    "§k": "\x1b[6m",
-};
-
-/**
- * Parses a native minecraft text component in string form.
- * @param {string} json_string - A text component string, such as the chat packet's "message" property.
- * @returns {object} Parsed message in { raw, clean, ansi } form.
- */
-function parseText(json_string) {
-    const json = JSON.parse(json_string);
-
-    let raw = parseJson(json, {color:"reset"});
-    if (raw.startsWith("§r")) {
-        raw = raw.substring(2);
-    }
-    let clean = raw.replace(/§[a-f0-9rlonmk]/g, "").replace(/§/g, "");
-    let ansi = raw.replace(/§[a-f0-9rlonmk]/g, (m) => {
-        return ansimap[m];
-    });
-    return { raw, clean, ansi };
-}
-
-/**
- * Parses a native minecraft text component in JSON form.
- * @param {object} json - The json message.
- * @param {object} parent - The parent json.
- * @returns {string} The parsed raw string.
- */
-function parseJson(json, parent) {
-    if (typeof json === "string") {
-        json = {text:json};
-    }
-
-    json.color = json.color || parent.color;
-    json.bold = json.bold || parent.bold;
-    json.italic = json.italic || parent.italic;
-    json.underlined = json.underlined || parent.underlined;
-    json.strikethrough = json.strikethrough || parent.strikethrough;
-    json.obfuscated = json.obfuscated || parent.obfuscated;
-
-    let raw = "";
-    raw += colormap[json.color] || "";
-    if (json.bold) { raw += "§l"; }
-    if (json.italic) { raw += "§o"; }
-    if (json.underlined) { raw += "§n"; }
-    if (json.strikethrough) { raw += "§m"; }
-    if (json.obfuscated) { raw += "§k"; }
-    if (json.text) {
-        raw += json.text;
-    }
-    if (json.translate) { // I checked with the native minecraft code. This is how Minecraft does the matching and group indexing. -hhhzzzsss
-        if (language[json.translate]) {
-            let _with = json.with || [];
-            let i = 0;
-            raw += language[json.translate].replace(/%(?:(\\d+)\\$)?(s|%)/g, (g0, g1) => {
-                if (g0 === "%%") {
-                    return "%";
-                }
-                else {
-                    let idx = g1 ? parseInt(g1) : i++;
-                    if (_with[idx]) {
-                        return parseJson(_with[idx], json);
-                    }
-                    else {
-                        return "";
-                    }
-                }
-            });
-        }
-        else {
-            raw += json.translate;
-        }
-    }
-    if (json.extra) {
-        json.extra.forEach(function (extra) {
-            raw += parseJson(extra, json);
-        });
-    }
-    return raw;
-}
+const      mc = require("minecraft-protocol"),
+           fs = require("fs"),
+eventemmitter = require('events'),
+         util = require("util"),
+         rand = require("randomstring"),
+        pchat = require("prismarine-chat")
 function createBot(config){
-    bot = {}
-    if(!config.username) config.username = "Liteflayer" + rand(3)
+    bot = new eventemmitter()
+    bot.position = {x: 0, y: 0, z: 0}
+    if(!config.username) config.username = "Liteflayer" + rand.generate(3)
     bot._client = mc.createClient(config)
     bot._client.on('position', (pos) => {bot.position = pos})
-    bot._client.on('message', (msg) => {bot.emit('message',parseText(parseJson(msg)))})
+    bot._client.on('message', (msg) => {
+        try{
+            bot.emit('message', new pchat.ChatMessage(msg))
+        } catch (e) {}
+    })
     bot.setCommandBlock = (command, pos, mode, flags) => bot._client.write('update_command_block',{command: command, position: pos, mode: mode, flags: flags})
     bot.chat = (send) => bot._client.write('chat',{message: send})
 }
