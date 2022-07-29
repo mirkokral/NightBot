3df037ae29d1b10746584f9fa41475c1f798315c
mirkokral <mirkogodak@gmail.com>
2022-07-29 09:54:16 +0100

    some more stuff

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 const liteflayer = require('./liteflayer')
-const config = require('yaml').parse(require('fs').readFileSync('./config.yml','utf-8'))
-var bots = []
+var config = require('yaml').parse(require('fs').readFileSync('./config.yml','utf-8'))
+const fs = require('fs')
+var bots = {}
+createBot = async (e) => {
+    bots[e.name] = await liteflayer.createBot({host: e.host, port: e.port})
+    await bots[e.name].on('message', (msg) => {
+        console.log(msg.toAnsi())
+    })
+    await bots[e.name]._client.on('error', (e) => {
+        console.log(e)
+        process.exit(1)
+    })
+    await bots[e.name]._client.on('end', () => createBot(e))
+    await bots[e.name].on('login', () => {
+        bots[e.name].chat("/essentials:evanish enable")
+        bots[e.name].chat("/extras:username &%1&%2&r&eNightBot".replace('%1',Math.floor(Math.random() * 9)).replace('%2',Math.floor(Math.random() * 9)).replace('%3',Math.floor(Math.random() * 9)))
+        config.plugins.forEach(e => {
+            bot.chat('&3[&b&lLOAD&3] &b%'.replace('%',e))
+            try{
+                eval(fs.readFileSync(`./plugins/${e}`,'utf-8'))
+                bot.chat('&2[&a&lOK&2] &b%'.replace('%',e))
+            } catch (e) {
+                bot.chat('&4[&c&lFAIL&4] &b%'.replace('%',e))
+            }
+        })
+        bots[e.name].chat("&6NightBot&7, a utility bot made by &#fafa6e&lm&#bdea75&li&#86d780&lr&#54c18a&lk&#23aa8f&lo&#00918d&lk&#007882&lr&#1f5f70&la&#2a4858&ll")
+    })
+}
+config.servers.forEach(createBot)
--- liteflayer.js
+++ liteflayer.js
@@ -3,19 +3,32 @@ const      mc = require("minecraft-protocol"),
 eventemmitter = require('events'),
          util = require("util"),
          rand = require("randomstring"),
-        pchat = require("prismarine-chat")
+        pchat = require("prismarine-chat")("1.16")
 function createBot(config){
     bot = new eventemmitter()
     bot.position = {x: 0, y: 0, z: 0}
     if(!config.username) config.username = "Liteflayer" + rand.generate(3)
     bot._client = mc.createClient(config)
-    bot._client.on('position', (pos) => {bot.position = pos})
-    bot._client.on('message', (msg) => {
+    bot._queue = []
+    bot._client.on('position', (pos) => {bot.emit('pos',pos)})
+    bot._client.on('chat', (msg) => {
         try{
-            bot.emit('message', new pchat.ChatMessage(msg))
-        } catch (e) {}
+            bot.emit('message', new pchat(JSON.parse(msg.message)))
+        } catch (e) {console.log(e)}
     })
-    bot.setCommandBlock = (command, pos, mode, flags) => bot._client.write('update_command_block',{command: command, position: pos, mode: mode, flags: flags})
-    bot.chat = (send) => bot._client.write('chat',{message: send})
+    bot._client.on('login', (msg) => {
+        try{
+            bot.emit('login')
+        } catch (e) {console.log(e)}
+    })
+    setInterval(() => {
+        if(bot._queue[0]){
+            bot._client.write("chat", {message: bot._queue[0]})
+            bot._queue.shift()
+        }
+    }, 100);
+    bot.setCommandBlock = (command, pos, mode, flags) => bot._client.write('update_command_block',{command: command, location: pos, mode: mode, flags: flags})
+    bot.chat = (send) => bot._queue.push(send)
+    return bot
 }
 module.exports = { createBot }
